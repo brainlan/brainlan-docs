67dfab10c103bbda7ef160f713b232ecdfa4b1ea
ohrrkan <ohrrkan@brainlan.com>
2024-02-20 19:09:56 +0100

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,30 @@
-import starlight from "@astrojs/starlight";
-import { defineConfig } from "astro/config";
-
-import vercel from "@astrojs/vercel/serverless";
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "My Docs",
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
-      sidebar: [
-        {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            {
-              label: "Example Guide",
-              link: "/guides/example/",
-            },
-          ],
-        },
-        {
-          label: "Reference",
-          autogenerate: {
-            directory: "reference",
-          },
-        },
-      ],
-    }),
-  ],
-  output: "hybrid",
-  adapter: vercel(),
+	integrations: [
+		starlight({
+			title: 'Docs with Tailwind',
+			social: {
+				github: 'https://github.com/withastro/starlight',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+			customCss: ['./src/tailwind.css'],
+		}),
+		tailwind({ applyBaseStyles: false }),
+	],
 });
